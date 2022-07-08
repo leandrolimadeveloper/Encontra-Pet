0b18b372a1d0af333c2d0458ccf1e1c160d96b02
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-07-08 15:48:27 -0300

    Register of users

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,9 @@
 import { Router } from 'express'
-import User from '../app/models/User'
+
+import UserController from '../app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => {
-    return res.json({ok: true})
-})
-
-routes.get('/teste', async (req, res) => {
-    const user = await User.create({
-        name: 'Leandro',
-        email: 'leandrolima@gmail.com',
-        password_hash: '1213123ab'
-    })
-
-    return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes;
