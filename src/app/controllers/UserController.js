5d6f31eb5660f701b80e62ae1e1a0069cb605864
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-07-11 18:40:40 -0300

    Updating user data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,9 +20,36 @@ class UserController {
     }
 
     async update(req, res) {
-        console.log(req.userId)
-        
-        res.json({ok: true})
+        const { email, password, oldPassword } = req.body
+
+        const user = await User.findByPk(req.userId)
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email }
+            })
+
+            if (userExists) {
+                return res.status(400).json({ error: 'Usuário já existe' })
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            
+            return res.status(401).json({ error: 'Senha atual incorreta' })
+        }
+
+        if (password === oldPassword) {
+            return res.status(400).json({error: 'Nova senha e senha anterior são as mesmas'})
+        }
+
+        const { id, name } = await user.update(req.body)
+
+        res.json({
+            id,
+            name,
+            email,
+        })
     }
 }
 
