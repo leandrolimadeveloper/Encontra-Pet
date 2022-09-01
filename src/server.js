302d197099b0d274a9c1a6c54416ee7653533136
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-09-01 18:34:21 -0300

    Starting first database connection, after server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ import app from './app'
 
 import database from './database'
 
-const port = 8686
-app.listen(port, err => {
-    err ? console.log(err) : console.log(`Server running at: http://localhost:${port}`)
-})
+if (database.init) {
+    console.log('Database started')
+    const port = 8686
+    app.listen(port, err => {
+        err ? console.log(err) : console.log(`Server running at: http://localhost:${port}`)
+    })
+}
+
