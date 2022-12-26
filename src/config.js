1d67f4963a4df2b61e2a01227faf1fc5b7aa0b53
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-12-26 11:21:20 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,21 +2,6 @@ export const PORT = process.env.PORT || 8686
 
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'postgres'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'senhaPG..2120'
-export const DB_NAME = process.env.DB_NAME || 'encontra_pet'
+export const DB_PASSWORD = process.env.DB_PASSWORD || process.env.PASSWORD
+export const DB_NAME = process.env.DB_NAME || process.env.DATABASE
 export const DB_PORT = process.env.DB_PORT || 5432
-
-// const DB_HOST = process.env.DB_HOST || 'localhost'
-// const DB_USER = process.env.DB_USER || 'postgres'
-// const DB_PASSWORD = process.env.DB_PASSWORD || 'senhaPG..2120'
-// const DB_NAME = process.env.DB_NAME || 'encontra_pet'
-// const DB_PORT = process.env.DB_PORT || 5432
-
-// module.exports = {
-//     DB_HOST,
-//     DB_USER,
-//     DB_PASSWORD,
-//     DB_NAME,
-//     DB_PORT,
-//     PORT
-
