09640309d905dc4ec02a6c3d6707709afca22a3e
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-01-03 09:37:17 -0300

    Update UserController.js - Update resource

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,103 +1,100 @@
-import * as Yup from 'yup'
-import User from '../models/User.js'
+import * as Yup from 'yup';
+import User from '../models/User.js';
 
 class UserController {
     async store(req, res) {
         const schema = Yup.object().shape({
             name: Yup.string().required(),
-            email: Yup.string()
-                .required()
-                .email(),
-            password: Yup.string()
-                .required()
-                .min(8)
-        })
+            email: Yup.string().required().email(),
+            password: Yup.string().required().min(8),
+        });
 
         try {
-            await schema.validate(req.body)
+            await schema.validate(req.body);
         } catch (err) {
             return res.status(400).json({
                 error: true,
-                message: err.errors
-            })
+                message: err.errors,
+            });
         }
 
         const userExists = await User.findOne({
-            where: { email: req.body.email }
-        })
+            where: { email: req.body.email },
+        });
 
         if (userExists) {
-            return res.status(400).json({ error: 'Usuário já existe' })
+            return res.status(400).json({ error: 'User already exists' });
         }
 
-        const { id, name, email } = await User.create(req.body)
+        const { id, name, email } = await User.create(req.body);
 
         return res.json({
             error: false,
-            message: 'Dados cadastrados com sucesso',
+            message: 'Data successfully registered',
             id,
             name,
-            email
-        })
+            email,
+        });
     }
 
     async update(req, res) {
         const schema = Yup.object().shape({
-            name: Yup.string().required(),
+            name: Yup.string(),
             email: Yup.string().email(),
             oldPassword: Yup.string().min(8),
             password: Yup.string()
                 .min(8)
                 .when('oldPassword', (oldPassword, field) => {
-                    oldPassword ? field.required() : field
+                    oldPassword ? field.required() : field;
                 }),
-                confirmPassword: Yup.string().when('password', (password, field) =>
-                    password ? field.required().oneOf([Yup.ref('password')]) : field
-                )
-        })
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
 
         try {
-            await schema.validate(req.body)
+            await schema.validate(req.body);
         } catch (err) {
             return res.status(400).json({
                 error: true,
-                message: err.errors
-            })
+                message: err.errors,
+            });
         }
 
-        const { email, password, oldPassword } = req.body
+        const { email, password, oldPassword } = req.body;
 
-        const user = await User.findByPk(req.userId)
+        const user = await User.findByPk(req.userId);
 
         if (email !== user.email) {
             const userExists = await User.findOne({
-                where: { email }
-            })
+                where: { email },
+            });
 
             if (userExists) {
-                return res.status(400).json({ error: 'Usuário já existe' })
+                return res.status(400).json({ error: 'User already exists' });
             }
         }
 
         if (oldPassword && !(await user.checkPassword(oldPassword))) {
-
-            return res.status(401).json({ error: 'Senha atual incorreta' })
+            return res
+                .status(401)
+                .json({ error: 'The current password is incorrect' });
         }
 
         if (password === oldPassword) {
-            return res.status(400).json({error: 'Nova senha e senha anterior são as mesmas'})
+            return res.status(400).json({ error: 'Passwords are the same' });
         }
 
-        const { id, name } = await user.update(req.body)
+        const { id, name } = await user.update(req.body);
 
         res.json({
             error: false,
-            message: 'Dados cadastrados com sucesso',
+            message: 'Data successfully updated',
             id,
             name,
             email,
-        })
+        });
     }
 }
 
-export default new UserController()
+export default new UserController();
